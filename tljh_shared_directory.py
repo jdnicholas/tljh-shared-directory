8d4def97676a122b612a73a478d1c08e5885f7d9
jdnicholas <jdnicholas10@gmail.com>
2022-12-14 15:34:03 +0100

    Changed folder name from scratch to sharing_folder

--- tljh_shared_directory.py
+++ tljh_shared_directory.py
@@ -9,21 +9,21 @@ def tljh_extra_user_conda_packages():
 @hookimpl
 def tljh_config_post_install(config):
     """
-    Configure /srv/scratch and change configs/mods
+    Configure /srv/sharing_folder and change configs/mods
     """
-    ### mkdir -p /srv/scratch
-    ### sudo chown  root:jupyterhub-users /srv/scratch
-    ### sudo chmod 777 /srv/scratch
-    ### sudo chmod g+s /srv/scratch
-    ### sudo ln -s /srv/scratch /etc/skel/scratch
+    ### mkdir -p /srv/sharing_folder
+    ### sudo chown  root:jupyterhub-users /srv/sharing_folder
+    ### sudo chmod 777 /srv/sharing_folder
+    ### sudo chmod g+s /srv/sharing_folder
+    ### sudo ln -s /srv/scratch /etc/skel/sharing_folder
     sh.mkdir('/srv/scratch', '-p')
     # jupyterhub-users doesn't get created until a user logs in
     # make sure it's created before changing permissions on directory
     ensure_group('jupyterhub-users') 
-    sh.chown('root:jupyterhub-users', '/srv/scratch')
-    sh.chmod('777', '/srv/scratch')
-    sh.chmod('g+s', '/srv/scratch')
-    sh.ln('-s', '/srv/scratch', '/etc/skel/scratch')
+    sh.chown('root:jupyterhub-users', '/srv/sharing_folder')
+    sh.chmod('777', '/srv/sharing_folder')
+    sh.chmod('g+s', '/srv/sharing_folder')
+    sh.ln('-s', '/srv/scratch', '/etc/skel/sharing_folder')
 
     
     
